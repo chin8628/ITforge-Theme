9c32e9603422ce432387c635b9ff1088d647326e
Boonyarith Piriyothinkul <boon.8628@hotmail.com>
2015-10-19 22:03:17 +0700

    git init
    none description

---
+++ inc/jetpack.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Jetpack Compatibility File.
+ *
+ * @link https://jetpack.me/
+ *
+ * @package ITforge
+ */
+
+/**
+ * Add theme support for Infinite Scroll.
+ * See: https://jetpack.me/support/infinite-scroll/
+ */
+function itforge_jetpack_setup() {
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'itforge_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+} // end function itforge_jetpack_setup
+add_action( 'after_setup_theme', 'itforge_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function itforge_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		get_template_part( 'template-parts/content', get_post_format() );
+	}
+} // end function itforge_infinite_scroll_render
