ae2869feb685c3d362d8166f0266a70e94622f34
Boonyarith Piriyothinkul <boon.8628@hotmail.com>
2015-10-21 23:03:15 +0700

    Add section
    add section for more dynamic to edit class it.

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -10,26 +10,32 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
-		<div class="entry-meta">
-			<?php itforge_posted_on(); ?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
+	<section class="container col-md-8 col-md-offset-2">
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'itforge' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
-	<footer class="entry-footer">
-		<?php itforge_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+			<div class="entry-meta">
+				<?php itforge_posted_on(); ?>
+			</div><!-- .entry-meta -->
+		</header><!-- .entry-header -->
+
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'itforge' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+
+		<footer class="entry-footer">
+			<?php itforge_entry_footer(); ?>
+		</footer><!-- .entry-footer -->
+
+	</section>
+
 </article><!-- #post-## -->
 
