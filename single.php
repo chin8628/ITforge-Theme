48913b702c19ff5833e8891fb62e72bfcdb8bdba
Boonyarith Piriyothinkul <boon.8628@hotmail.com>
2015-10-22 12:00:41 +0700

    Edit size single post
    Add section class container and size 8 by bootstrap css

--- single.php
+++ single.php
@@ -12,20 +12,24 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+		<section class="container col-md-8 col-md-offset-2">
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php the_post_navigation(); ?>
+				<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
+				<?php the_post_navigation(); ?>
 
-		<?php endwhile; // End of the loop. ?>
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -11,31 +11,27 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<section class="container col-md-8 col-md-offset-2">
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
-		<header class="entry-header">
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<div class="entry-meta">
+			<?php itforge_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
 
-			<div class="entry-meta">
-				<?php itforge_posted_on(); ?>
-			</div><!-- .entry-meta -->
-		</header><!-- .entry-header -->
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'itforge' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
 
-		<div class="entry-content">
-			<?php the_content(); ?>
-			<?php
-				wp_link_pages( array(
-					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'itforge' ),
-					'after'  => '</div>',
-				) );
-			?>
-		</div><!-- .entry-content -->
-
-		<footer class="entry-footer">
-			<?php itforge_entry_footer(); ?>
-		</footer><!-- .entry-footer -->
-
-	</section>
+	<footer class="entry-footer">
+		<?php itforge_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
 
 </article><!-- #post-## -->
 
