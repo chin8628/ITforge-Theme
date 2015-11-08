bf7877db38bc82955f25e95f30632e52529f07ef
Boonyarith Piriyothinkul <boon.8628@hotmail.com>
2015-11-08 08:30:02 +0700

    move slider image from header to index

--- index.php
+++ index.php
@@ -14,78 +14,83 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area col-lg-8 col-lg-offset-2 container">
-		<main id="main" class="site-main" role="main">
+    <!--
+        Paste usage code from MetaSlider here.
+        Warning!! Not short code. use php code.
+    -->
 
-			<?php
-				/*
-					fetch list of category into array for display on index
-					$category keep name_slug of category
-				*/
-				$index = 0;
-				$var = get_categories('orderby=slug');
-				foreach ($var as $value) {
-					$category[$index] = $value->slug;
-					$category_id[$index] = $value->cat_ID;
-					$index++;
-				}
+    <div id="primary" class="content-area col-lg-8 col-lg-offset-2 container">
+        <main id="main" class="site-main" role="main">
 
-				//$total_category keep total number of category
-				$exploded = explode('<br />', wp_list_categories('title_li=&style=none&echo=0&depth=1'));
-				$total_category = count($exploded) - 1;
+            <?php
+                /*
+                    fetch list of category into array for display on index
+                    $category keep name_slug of category
+                */
+                $index = 0;
+                $var = get_categories('orderby=slug');
+                foreach ($var as $value) {
+                    $category[$index] = $value->slug;
+                    $category_id[$index] = $value->cat_ID;
+                    $index++;
+                }
 
-			?>
+                //$total_category keep total number of category
+                $exploded = explode('<br />', wp_list_categories('title_li=&style=none&echo=0&depth=1'));
+                $total_category = count($exploded) - 1;
 
-			<?php $count = 0; ?>
+            ?>
 
-			<?php for ($i = 1; $i <= ceil($total_category / 2); $i++){ ?>
+            <?php $count = 0; ?>
 
-			<section class="box-category row">
+            <?php for ($i = 1; $i <= ceil($total_category / 2); $i++){ ?>
 
-				<?php if ($total_category % 2 != 0 && $i == ceil($total_category / 2)) $var = 1; else $var = 2; ?>
-				<?php for ($j = 1; $j <= $var; $j++){ ?>
+            <section class="box-category row">
 
-				<div class="col-md-6 index">
+                <?php if ($total_category % 2 != 0 && $i == ceil($total_category / 2)) $var = 1; else $var = 2; ?>
+                <?php for ($j = 1; $j <= $var; $j++){ ?>
 
-					<?php query_posts( array('category_name' => $category[$count], 'showposts' => 4) ); ?>
-					<h2 class="bangna"> <?php single_cat_title() ?> </h2>
-					<?php while ( have_posts() ) : the_post(); ?>
-						<div class="box-article row">
-							<section class="col-md-6 col-sm-6">
-								<a href="<?php the_permalink(); ?>">
-								<?php if ( has_post_thumbnail() ) : ?>
-									<?php the_post_thumbnail(); ?>
-								<?php else: ?>
-									<img src="<?php bloginfo('template_directory'); ?>/asset/img/blank.jpg" />
-								<?php endif; ?>
-								</a>
-							</section>
-							<section class="col-md-6 col-sm-6">
-								<a href="<?php the_permalink(); ?>">
-									<p class="title"><strong> <?php echo get_the_title(); ?> </strong></p>
-								</a>
-								<small><?php the_date(); ?></small>
-							</section>
-						</div>
-					<?php endwhile; ?>
-					<?php echo '<a href="'. get_category_link( $category_id[$count] ) .'">อ่านทั้งหมด</a>'; ?>
+                <div class="col-md-6 index">
 
-				</div>
+                    <?php query_posts( array('category_name' => $category[$count], 'showposts' => 4) ); ?>
+                    <h2 class="bangna"> <?php single_cat_title() ?> </h2>
+                    <?php while ( have_posts() ) : the_post(); ?>
+                        <div class="box-article row">
+                            <section class="col-md-6 col-sm-6">
+                                <a href="<?php the_permalink(); ?>">
+                                <?php if ( has_post_thumbnail() ) : ?>
+                                    <?php the_post_thumbnail(); ?>
+                                <?php else: ?>
+                                    <img src="<?php bloginfo('template_directory'); ?>/asset/img/blank.jpg" />
+                                <?php endif; ?>
+                                </a>
+                            </section>
+                            <section class="col-md-6 col-sm-6">
+                                <a href="<?php the_permalink(); ?>">
+                                    <p class="title"><strong> <?php echo get_the_title(); ?> </strong></p>
+                                </a>
+                                <small><?php the_date(); ?></small>
+                            </section>
+                        </div>
+                    <?php endwhile; ?>
+                    <?php echo '<a href="'. get_category_link( $category_id[$count] ) .'">อ่านทั้งหมด</a>'; ?>
 
-				<?php $count++; ?>
-				<?php } ?>
+                </div>
 
-			</section><!-- end category box -->
+                <?php $count++; ?>
+                <?php } ?>
 
-			<?php if ($i != ceil($total_category / 2)){ ?>
-				<hr class="bold">
-			<?php } ?>
+            </section><!-- end category box -->
 
-			<?php } ?>
+            <?php if ($i != ceil($total_category / 2)){ ?>
+                <hr class="bold">
+            <?php } ?>
 
-			<?php require('footer-content.php'); ?>
+            <?php } ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+            <?php require('footer-content.php'); ?>
+
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php get_footer(); ?>
